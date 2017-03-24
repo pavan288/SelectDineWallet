d1544d50e17ad07fe9e0cee695baaa5cf19fa501
Pavan Powani <pjp288@gmail.com>
2017-03-24 18:35:02 +0530

    first working fb login model

--- SelectDineWallet/ViewController.swift
+++ SelectDineWallet/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        view.addSubview(loginButton)
+        loginButton.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //methods for facebook login
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print("Error logging in:\(error)")
+            return
+        }
+        print("Successfully logged in")
     }
-
+   
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Successfully logged out")
+    }
+    //end of methods for facebook login
 
 }
 
