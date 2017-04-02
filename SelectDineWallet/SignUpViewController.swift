beef20a3b164a9b4ffd140e62b45925ab58bee4e
Pavan Powani <pjp288@gmail.com>
2017-04-02 12:35:13 +0530

    sign up disabled until phone verification. verify button handled for errors

--- SelectDineWallet/SignUpViewController.swift
+++ SelectDineWallet/SignUpViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SwiftyJSON
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController,UITextViewDelegate {
 
     @IBOutlet var name: UITextField!
     @IBOutlet var email: UITextField!
@@ -17,11 +17,15 @@ class SignUpViewController: UIViewController {
     @IBOutlet var phone: UITextField!
     
     var userID: String! = nil
-    
+    var verificationFlag: Bool = false
+    @IBOutlet var signUpButton: UIButton!
     let prefs = UserDefaults.standard
+    @IBOutlet var verifyButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        signUpButton.isEnabled = false
         // Do any additional setup after loading the view.
         
     }
@@ -42,12 +46,7 @@ class SignUpViewController: UIViewController {
         
         let message = readableJSON["message"].string! as String
         let status = readableJSON["status"].int! as Int
-        let emailID = readableJSON["email"].string! as String
-        let mobileNo = readableJSON["mobileNo"]
         userID = readableJSON["userid"].string! as String
-        prefs.set(userID, forKey: "userID")
-        prefs.set(email, forKey: "email")
-        prefs.set(mobileNo, forKey: "phone")
         print("\(status):\(message)")
         
         let alert = UIAlertController(title: "Sign Up", message: "\(message)", preferredStyle: .actionSheet)
@@ -63,8 +62,8 @@ class SignUpViewController: UIViewController {
     }
 
     @IBAction func verifyPhoneNumber(_ sender: Any) {
-        let mobile = Int(phone.text!)
-        let urlpath = "http://35.154.46.78:1337/otp/generateOtpForMobileVerificationDuringSignup?mobileNo=\(mobile!)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        if let mobile = Int(phone.text!){
+        let urlpath = "http://35.154.46.78:1337/otp/generateOtpForMobileVerificationDuringSignup?mobileNo=\(mobile)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         let url = URL(string: urlpath!)
         
         let jsonData = try? Data(contentsOf: url! as URL, options: [])
@@ -85,12 +84,14 @@ class SignUpViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
             let otpValue = alert.textFields![0].text!
             print(otpValue)
-            let urlpath = "http://35.154.46.78:1337/otp/verifyMobileNoWhileSignup?mobileNo=\(mobile!)&otp=\(otpValue)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            let urlpath = "http://35.154.46.78:1337/otp/verifyMobileNoWhileSignup?mobileNo=\(mobile)&otp=\(otpValue)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
             let url = URL(string: urlpath!)
             let jsonData = try? Data(contentsOf: url! as URL, options: [])
             let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
             let message = readableJSON["message"].string! as String
             print(message)
+            
+            self.signUpButton.isEnabled = true
             
         })
         alert.addTextField { (textfield:UITextField) -> Void in
@@ -105,14 +106,6 @@ class SignUpViewController: UIViewController {
         self.present(alert, animated: true)
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
 }
