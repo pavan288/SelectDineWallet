2ba72f459b99eb12adcac298e1580b79c6fb4a71
Pavan Powani <pjp288@gmail.com>
2017-04-08 22:32:07 +0530

    url paths rehandled

--- SelectDineWallet/PayViewController.swift
+++ SelectDineWallet/PayViewController.swift
@@ -42,14 +42,15 @@ class PayViewController: UIViewController {
 
     @IBAction func confirmPressed(_ sender: UIButton) {
         if phoneFlag == 1{
-            payUser()
+            transferToBank()
         }else if phoneFlag == 0{
-            
-        receivePayment()
-    }
+            payUser()
+        }else if phoneFlag == 2{
+            payUser()
+        }
     }
     
-    func payUser(){
+    func transferToBank(){
         if let number = phNumber.text {
             let urlpath = "http://35.154.46.78:1337/payment/ifPayWithReceiversMobileNo?mobileNo=\(Int(number)!)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
             let url = URL(string: urlpath!)
@@ -71,8 +72,47 @@ class PayViewController: UIViewController {
                 let message = readableJSON["message"].string! as String
                 print(status)
                 print(message)
+                
+                if status == 5657{
+                    let urlpath = "http://35.154.46.78:1337/payment/walletToBankTransfer?userId=\(userid)&amount=\(amount.text!)&reason=\(reason.text!)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                    let url = URL(string: urlpath!)
+                    
+                    let jsonData = try? Data(contentsOf: url! as URL, options: [])
+                    let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+                    let status = readableJSON["status"].int! as Int
+                    let message = readableJSON["message"].string! as String
+                    print(status)
+                    print(message)
+                    
+                    if status == 6602{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                            // perhaps use action.title here
+                        })
+                        self.present(alert, animated: true)
+                    }else if status == 611{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                            // perhaps use action.title here
+                        })
+                        self.present(alert, animated: true)
+                    }else if status == 613{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                            // perhaps use action.title here
+                        })
+                        self.present(alert, animated: true)
+                    }
+                }else if status == 5655{
+                    let alert = UIAlertController(title: "Error!", message: "Cannot find user", preferredStyle: .actionSheet)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                    })
+                    self.present(alert, animated: true)
+                }
+                
+                
             }else{
-                let alert = UIAlertController(title: "Login", message: "Cannot find user", preferredStyle: .actionSheet)
+                let alert = UIAlertController(title: "Error", message: "User not found", preferredStyle: .actionSheet)
                 alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
                     // perhaps use action.title here
                 })
@@ -88,27 +128,76 @@ class PayViewController: UIViewController {
         }
     }
    
-    func receivePayment(){
-        if let amt = Int(amount.text!){
-        let urlpath = "http://35.154.46.78:1337/payment/walletToBankTransfer?userId=\(prefs.value(forKey: "userID"))&amount=\(amt)&reason=".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        let url = URL(string: urlpath!)
-        
-        let jsonData = try? Data(contentsOf: url! as URL, options: [])
-        let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
-        
-        let message = readableJSON["message"].string! as String
-        
-        print(prefs.value(forKey: "userID")!)
-        print(message)
+    func payUser(){
+        if let number = phNumber.text , let amt = amount.text {
+            let reason = self.reason.text!
+            if (number != "") && (amt != ""){
+             let urlpath = "http://35.154.46.78:1337/payment/ifPayWithReceiversMobileNo?mobileNo=\(number)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            let url = URL(string: urlpath!)
+            let jsonData = try? Data(contentsOf: url! as URL, options: [])
+            let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+            let status = readableJSON["status"].int! as Int
+            let userid = readableJSON["userId"].string! as String
+            
+            if(status == 597){
+                let urlpath = "http://35.154.46.78:1337/payment/paymentReceiveBegin?receiverId=\(userid)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                let url = URL(string: urlpath!)
+                let jsonData = try? Data(contentsOf: url! as URL, options: [])
+                let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+                
+                let status = readableJSON["status"].int! as Int
+                let message = readableJSON["message"].string! as String
+                print(status)
+                print(message)
+                
+                if status == 5657{
+                    let urlpath = "http://35.154.46.78:1337/payment/walletToBankTransfer?userId=\(userid)&amount=\(amt)&reason=\(reason)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                    let url = URL(string: urlpath!)
+                    let jsonData = try? Data(contentsOf: url! as URL, options: [])
+                    let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+                    let status = readableJSON["status"].int! as Int
+                    let message = readableJSON["message"].string! as String
+                    print(status)
+                    print(message)
+                    
+                    if status == 6602{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                            // perhaps use action.title here
+                        })
+                        self.present(alert, animated: true)
+                    }else if status == 611{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                        })
+                        self.present(alert, animated: true)
+                    }else if status == 613{
+                        let alert = UIAlertController(title: "Error!", message: "\(message)", preferredStyle: .actionSheet)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                        })
+                        self.present(alert, animated: true)
+                    }
+                }else if status == 5655{
+                    let alert = UIAlertController(title: "Error!", message: "Cannot find user", preferredStyle: .actionSheet)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                    })
+                    self.present(alert, animated: true)
+                }
+            }else{
+                let alert = UIAlertController(title: "Error!", message: "Cannot find user", preferredStyle: .actionSheet)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                })
+                self.present(alert, animated: true)
+            }
         }else{
-            let alert = UIAlertController(title: "Login", message: "Please enter an amount", preferredStyle: .actionSheet)
+            let alert = UIAlertController(title: "Error!", message: "Please enter all the fields", preferredStyle: .actionSheet)
             alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
-                // perhaps use action.title here
             })
             self.present(alert, animated: true)
         }
     }
-    
+    }
+
     /*
     // MARK: - Navigation
 
