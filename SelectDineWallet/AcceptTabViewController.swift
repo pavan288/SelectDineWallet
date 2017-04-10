d8a3dd6cc6f8f0ca3d3cb846a3f60f43e7db03e2
Pavan Powani <pjp288@gmail.com>
2017-04-10 23:09:06 +0530

    if online, parses qr code, else retrieves form local storage

--- SelectDineWallet/AcceptTabViewController.swift
+++ SelectDineWallet/AcceptTabViewController.swift
@@ -33,14 +33,14 @@ class AcceptTabViewController: UIViewController,UIWebViewDelegate {
             if let urlpath = "http://35.154.46.78:1337/qrcode/generateqrcodeforios?id=\(uid!)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed){
         let url = URL(string: urlpath)
         
-        let jsonData = try? Data(contentsOf: url! as URL, options: [])
-        let readableJSON = JSON(data: jsonData! as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+                if let jsonData = try? Data(contentsOf: url! as URL, options: []){
+        let readableJSON = JSON(data: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
         
         qrdata = readableJSON["code"].string! as String
         prefs.set(qrdata, forKey: "qrCodeString")
         //print(qrdata!)
-        }
-        }
+        
+        
         let newurl = URL(string : qrdata)!
         // It Will turn Into Data
         let imageData : NSData = NSData.init(contentsOf: newurl as URL)!
@@ -52,11 +52,20 @@ class AcceptTabViewController: UIViewController,UIWebViewDelegate {
         let dataImage = UIImage(data: data as Data)
         // pass the data image to image View.:)
         testImage.image = dataImage
-        
+                }else{
+                    let existingQr = prefs.value(forKey: "qrCodeString")
+                    let newurl = URL(string : existingQr as! String)!
+                    let imageData : NSData = NSData.init(contentsOf: newurl as URL)!
+                    let str64 = imageData.base64EncodedData(options: .lineLength64Characters)
+                    let data: NSData = NSData(base64Encoded: str64 , options: .ignoreUnknownCharacters)!
+                    let dataImage = UIImage(data: data as Data)
+            
+                    testImage.image = dataImage
+                }
     }
-    
+}
 }
 
 
-
+}
 
