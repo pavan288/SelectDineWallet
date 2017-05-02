ce1dfe2eea10ddfefe3cf404992fb8b93cb0a70e
Pavan Powani <pjp288@gmail.com>
2017-05-02 22:54:41 +0530

    feedback complete

--- SelectDineWallet/FeedbackViewController.swift
+++ SelectDineWallet/FeedbackViewController.swift
@@ -7,20 +7,59 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class FeedbackViewController: UIViewController {
 
+    
+    @IBOutlet var feedbackText: UITextView!
+    let baseUrl = "http://35.154.46.78:1337"
+    let prefs = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-self.hideKeyboard()
+        self.hideKeyboard()
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func dismissVC(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendPressed(_ sender: Any) {
+        let uid = prefs.value(forKey: "userID")
+        let urlpath = "\(baseUrl)/feedback/createFeedback?id=\(uid)&feedback=\(feedbackText.text!)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: urlpath!)
+        
+        if let jsonData = try? Data(contentsOf: url! as URL, options: []){
+            let readableJSON = JSON(data: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
+            
+            let status = readableJSON["status"].int! as Int
+            
+            if(status == 332){
+                let alert = UIAlertController(title: "Success!", message: "Succesfully sent feedback", preferredStyle: .actionSheet)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                    // perhaps use action.title here
+                })
+                self.present(alert, animated: true)
+            }else{
+                let alert = UIAlertController(title: "Error!", message: "Could not send feedack", preferredStyle: .actionSheet)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+                    // perhaps use action.title here
+                })
+                self.present(alert, animated: true)
+            }
+        }else{
+            print("could not connect")
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
